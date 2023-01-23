9042fd5f6b35052823103f3a9eb0807084131dba
moa <lagneskog.moa@elev.ga.ntig.se>
2023-01-23 11:05:47 +0100

    First few codes

---
+++ src/Book.java
@@ -0,0 +1,13 @@
+public class Book {
+    String name;
+    Author author;
+    double price;
+    int qty = 0;
+
+public Book(String name, Author author, double price, int qty){
+    this.name = name;
+    this.author = author;
+    this.price = price;
+    this.qty = qty;
+}
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+Author MB = new Author("Michelle Barack", "michelle.barack@gmail.com", 'f');
+Author MT = new Author("Mike Tyson", "mike.tyson@gmail.com", 'm');
+Book MB1 = new Book("The Mouse", MB, 25, 100);
+Book MB2 = new Book("Rayan's Big Bunda", MB, 30, 1000000);
+Book MT1 = new Book("Suss's secret  recipe", MT, 20, 200);
+Book MT2 = new Book("Sevil and TE20E", MT, 10, 5);
+    }
+}
